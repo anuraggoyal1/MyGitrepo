e61c2eef56f9177499eae6b018e87af384059218
Anurag <anurag.alw@gmail.com>
2018-03-24 20:32:07 +0530

    added all

---
+++ myStockAlert/app/src/main/java/com/invest/stock/mystockalert/MyFirebaseInstanceIDService.java
@@ -0,0 +1,37 @@
+package com.invest.stock.mystockalert;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by gpq5 on 23/12/17.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("stockAlert");
+
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+}
